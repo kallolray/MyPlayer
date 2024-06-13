54db0cf892ececa3ed998a009675340acd7a7a87
Kallol Ray <rayk@timken.com>
2024-06-13 11:51:02 +0530

    Change1

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def test1():
     r = requests.get('https://api.github.com/users/kallolray')
     return r.content
  
+@app.route('/test2')
+def test2():
+    r = requests.get('https://music.youtube.com/watch?v=SW2uyfNqHg4')
+    return r.content
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
